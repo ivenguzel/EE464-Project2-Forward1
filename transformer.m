48226a7939eec90adfa5cb1a39f31d5d0b156c9e
Furkan <tokgoz.furkan@metu.edu.tr>
2019-03-26 23:44:31 +0300

    copper loss, core loss calculation fixed for FOR#4

--- transformer.m
+++ transformer.m
@@ -1,30 +1,36 @@
-P=21;                                                       %Watts
-J=500;                                                      %circ.mils/amp or 400 A/cm^2
-K=0.0005;                                                   %constant 0.0005 for forward converter
-B=2100;                                                     %max flux density for 100mW/cm^3 core loss in 30 kHz Gauss
-f=30000;                                                    %frequency Hz
-Vpmax=48;                                                   %maximum primary voltage V
-Vpmin=24;                                                   %minimum primary voltage V
-Ae=0.971;                                                   %cross section area for 0P43434EC cm^2
-Vcore=7.64;                                                 %volume of the core in cm^3
-Nt=3;                                                       %tertiary winding number of turns
-A_17=0.0104;                                                %AWG 17 copper area in cm^2
-R_17=16.61;
-A_16=0.0131;
-R_16=13.17;
-A_15=0.0165;
-R_15=10.45;
-Window_3434=1.21/Ae;                                        %window area of 0P43434EC in cm^2
+P=41;                   %Watts
+J=750;                  %circ.mils/amp or 400 A/cm^2
+K=0.0005;               %constant 0.0005 for forward converter
+B=1300;                 %max flux density for 100mW/cm^3 core loss in 30 kHz Gauss
+f=30000;                %frequency Hz
+Vpmax=48;               %maximum primary voltage V
+Vpmin=24;               %minimum primary voltage V
+Vout=10;                %output voltage
+Iout=3;                 %outout current
+Ae=0.971;               %cross section area for 0P43434EC cm^2
+Vcore=7.64;             %volume of the core in cm^3
+Window_3434=1.21/Ae;    %window area of 0P43434EC in cm^2
+T=50;                   %ambient temperature
+Nt=3;                   %reset winding number of turns
+A_25=0.00162;           %AWG 25 copper area in cm^2
+R_25=106.2;             %mohm/m for AWG25
+A_17=0.0104;            %AWG 17 copper area in cm^2
+R_17=16.61;             %mohm/m for AWG17
+A_16=0.0131;            %AWG 16 copper area in cm^2
+R_16=13.17;             %mohm/m for AWG16
+A_15=0.0165;            %AWG 15 copper area in cm^2
+R_15=10.45;             %mohm/m for AWG15
 
-WA=P*J/(K*B*f);                                             %Area product 
-Np=round(Vpmax*1e08/(4*B*f*Ae));                            %primary number of turns
-Ns=round((6*Np)/(Vpmin*0.5));                               %secondary number of turns
-Pc=Vcore*0.158*((f/1000)^1.36)*((B/1000)^2.86)/1000;        %Pc in W, f in kHz, B in kG
-Le=(((0.256/2-0.111/2)/4)+0.111/2)*2*pi;                    %calculation of mean length per turn in m
-Lcop=Le*(Np+Ns+Nt);                                         %total copper length in m
-Kwind=A_15*(Np+Ns+Nt)/Window_3434;                          %winding factor of 0P43434EC
-Rcop=R_15*Lcop;                                            %total dc resistance of copper in mOhm
-Pcop=Rcop*16/1000;                                          %copper loss in Watts
+
+WA=P*J/(K*B*f);                                                                 %Area product 
+Np=round(Vpmax*1e08/(4*B*f*Ae));                                                %primary number of turns
+Ns=round(((Vout+1)*Np)/(Vpmin*0.5));                                            %secondary number of turns
+Pcore=Vcore*3.2*(f^1.46)*((B/10000)^2.75)*(2.45-0.031*T+0.000165*T^2)*1e-06;    %core loss in W, f in Hz, B in G
+mlt=(((0.0256/2-0.0111/2)/2)+0.0111/2)*2*pi;                                    %calculation of mean length per turn of the cable in m
+Lcop=mlt*(Np+Ns+Nt);                                                            %total cable length in m
+Kwind=(A_17*Np+A_15*Ns+A_25*Nt)/Window_3434;                                              %winding factor of 0P43434EC
+Rcop=R_15*Lcop;                                                                 %total dc resistance of copper in mOhm
+Pcop=Rcop*(Iout^2)/1000;                                                        %copper loss in Watts
 
 
 
